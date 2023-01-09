37f32632e2e244d4d5b3960a7d64effd3d09e907
Denis Dolgopolov <jlomuk1990@gmail.com>
2023-01-09 09:10:50 +0300

    fix jwt expire token

--- user_service/src/settings.py
+++ user_service/src/settings.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str
     JWT_REFRESH_SECRET_KEY: str
 
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60*99999  # 3 hours
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 3   # 3 hours
     REFRESH_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 3  # 3 days
     ALGORITHM: str = "HS256"
 
